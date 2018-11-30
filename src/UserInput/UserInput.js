79aa3a3172046cd14e4b16df19ee4cb8f0fe0c72
Grace Averis <graceaveris@gmail.com>
2018-11-30 16:19:30 +0800

    last item styling

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 const userInput = (props) => {
+
+	const style = {
+   	border: '4px solid blue',
+   	backgroundColor: 'yellow',
+   	fontSize: '40px',
+   	textAlign: 'center',
+   	margin: '15px',
+   	width: '250px'
+   };
+
   
  return (
 
  	<input type="text" 
+ 	style={style}
  	value={props.value}
  	onChange={props.changed}/>
 
--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './UserOutput.css';
 
 const userOutput = ( props ) => {
  
   return (
    
-   <div>
-	  <p>Im cool!</p>
+   <div className="UserOutput">
+	  <p>
+A pardon brackets your small wonder. Should a pedal peripheral sand the hack incompetence? An encouraging pardon stores the delighted radius. Why won't each mill intervene beside the enhanced diesel? An advice invokes the faucet beneath the hypocritical constitutional.</p>
 	  <p>Author: {props.username}</p>
    </div>
 
